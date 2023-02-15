1c0f3da0450df9bc4a2a05e212672cdcf4ba7007
5-23 <yhanbyeol6@gmail.com>
2023-02-15 22:51:15 +0900

    index bug fix

--- script/all.js
+++ script/all.js
@@ -1,13 +1,1 @@
-// let observer = new IntersectionObserver((e)=>{
-//     e.forEach(box=>{
-//         if (box.isIntersecting){
-//             box.target.style.fontSize = "70px"
-//             box.target.style.transform = "translateY(35px)"
-//             box.target.style.opacity = "1"
-//         }else{
-//             // box.target.style.backgroundPositionX = "-400px";
-//         }
-//     })
-// })
-// let e = document.querySelectorAll("#title");
-// observer.observe(e[0]);
+document.querySelector("header > img").addEventListener("click", () => {location.href = "/"})
