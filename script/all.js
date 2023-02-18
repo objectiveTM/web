d84be3f21eefb166d65a3998abfce16a8fe29eca
5-23 <yhanbyeol6@gmail.com>
2023-02-18 21:56:55 +0900

    remake bots.html

--- script/all.js
+++ script/all.js
@@ -1,1 +1,10 @@
 document.querySelector("header > img").addEventListener("click", () => {location.href = "/"})
+
+document.querySelector("nav > a#one")
+    .addEventListener("click", () => {location.href = "/bots.html"})
+
+document.querySelector("nav > a#two")
+    .addEventListener("click", () => {location.href = "/team.html"})
+
+document.querySelector("nav > a#three")
+    .addEventListener("click", () => {location.href = "https://discord.gg/w2Fw7UeZmY"})
