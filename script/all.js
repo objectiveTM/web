8c3a9aed2f9931fc0c41a4854bbb4a7efd0c5705
Lyn Han <103019970+LynHan1110@users.noreply.github.com>
2023-02-19 23:18:29 +0900

    Update all.js

--- script/all.js
+++ script/all.js
@@ -6,5 +6,5 @@ document.querySelector("nav > a#one")
 document.querySelector("nav > a#two")
     .addEventListener("click", () => {location.href = "/team.html"})
 
-document.querySelector("nav > a#three")
-    .addEventListener("click", () => {location.href = "https://discord.gg/w2Fw7UeZmY"})
+    document.querySelector("nav > a#three")
+    .addEventListener("click", () => {location.href = "/discord.html"})
